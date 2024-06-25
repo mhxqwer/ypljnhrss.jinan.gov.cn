3e895f7addfb1a8d7882f6454436fa7d5a37651d
mhxqwer <168000686+mhxqwer@users.noreply.github.com>
2024-06-25 22:50:15 +0800

    Add files via upload

---
+++ static/js/1908211332212984.js
@@ -0,0 +1,34 @@
+document.writeln("<style>");
+document.writeln("	.clearfix:after { content:\"\";display:block;clear:both;height:0px;}");
+document.writeln(".clearfix{zoom:1;}");
+document.writeln(".wrapper{width: 1200px;margin:auto;}");
+document.writeln("</style>");
+document.writeln("<div class=\"rs_foot\" ergodic=\"serve\" style=\"height: 100px;width: 100%;background-color: #3687c9;\">");
+document.writeln("		<div class=\"wrapper clearfix\" style=\"height: 100%; position: relative;overflow: hidden;\">");
+document.writeln("			<div style=\"position: absolute;left: 0;top: 20px;\" class=\"media_none\">");
+document.writeln("				<!-- <script src=\"http://dcs.conac.cn/js/16/234/0000/60223101/CA162340000602231010002.js\" type=\"text/javascript\">");
+document.writeln("                </script> -->");
+document.writeln("                <span id=\"_ideConac\"><a href=\"http://bszs.conac.cn/sitename?method=show&id=097002628FB135FEE053022819AC9705\" target=\"_blank\"><img id=\"imgConac\" vspace=\"0\" hspace=\"0\" border=\"0\" src=\"http://dcs.conac.cn/image/red.png\" data-bd-imgshare-binded=\"1\"></a></span>");
+document.writeln("			</div>");
+document.writeln("			<div style=\"text-align: center;color: #fff;font-size: 15px;\">");
+document.writeln("				<p style=\"margin-top: 20px;\"><span style=\"padding-right:15px;\">版权所有：济南市人力资源和社会保障局</span><span class=\"media_none\">主办：济南市人力资源和社会保障局</span></p>");
+document.writeln("				<p style=\"margin:6px 0;\"><span class=\"media_none\">网站标识码：3701000056</span><img class=\"media_none\" style=\"padding:0 12px;margin-top: -1px;\" src=\"http://jnhrss.jinan.gov.cn/picture/443/1908211622017629908.png\" alt=\"\"><a class=\"media_none\" href=\"http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=37010202001191\" target=\"_blank\" style=\"color: #fff;font-size: 15px;\">鲁公网安备 37010202001191号</a><a href=\"https://beian.miit.gov.cn\" target=\"_blank\" style=\"color: #fff;font-size: 15px;padding-left: 15px;\">鲁ICP备10200122号</a></p>");
+document.writeln("				<p class=\"media_none\"><span>地址：济南市历下区龙鼎大道1号龙奥大厦5层</span><span style=\"padding: 0 15px;\">邮编：250099</span><a href=\"http://jnhrss.jinan.gov.cn/col/col18638/index.html\" target=\"_blank\" style=\"color: #fff;font-size: 15px;\" class=\"media_none\">网站地图</a></p>");
+document.writeln("			</div>");
+document.writeln("			<div style=\"position: absolute;right: 0;top: 26px;\" class=\"media_none\">");
+document.writeln("				<script id=\"_jiucuo_\" sitecode=\"3701000056\" src=\"http://pucha.kaipuyun.cn/exposure/jiucuo.js\">");
+document.writeln("                </script>");
+document.writeln("                <span id=\"_span_jiucuo\"></span>");
+document.writeln("			</div>");
+document.writeln("		</div>");
+document.writeln("	</div>");
+
+
+var juba_uuid = "";(function(){
+    var juba = document.createElement("script");
+    juba.type = "text/javascript";
+    juba.async = true;
+    juba.src = "http://221.214.69.232:31491/jpaas-juba-front-server/sync/detailcollect.do";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(juba,s);
+})();
